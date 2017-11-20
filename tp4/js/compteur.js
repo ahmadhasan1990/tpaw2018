408c63519ede28793123fedfc7620b58b4ceb953
ahmadhasan1990 <32831712+ahmadhasan1990@users.noreply.github.com>
2017-11-20 23:46:47 +0100

    Add files via upload

---
+++ tp4/js/compteur.js
@@ -0,0 +1,11 @@
+function resteN(texteN) {
+     document.getElementById('n_nom').innerHTML=texteN.length+' car';
+
+                        }
+
+  function resteP(texteP) {
+    document.getElementById('n_prenom').innerHTML=texteP.length+' car';
+                       
+                                               }
+
+
